03a4a51a89d6351e7bbbc4eb72fce2bf5930d9b8
stephenshank <sshank314@gmail.com>
2019-12-28 18:17:01 -0500

    Parse scripts associated with a subject.

--- api/srcs/admin.py
+++ api/srcs/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 from .models import Subject
+from .models import Package
+from .models import Module
+from .models import Script
 
 admin.site.register(Subject)
+admin.site.register(Package)
+admin.site.register(Module)
+admin.site.register(Script)
 
--- api/srcs/models.py
+++ api/srcs/models.py
@@ -1,3 +1,6 @@
+import os
+
+import ast
 from django.db import models
 from django.utils.text import slugify
 
@@ -5,5 +8,49 @@ from django.utils.text import slugify
 class Subject(models.Model):
     name = models.CharField(null=True, max_length=255, blank=True)
     token = models.CharField(null=True, max_length=255, blank=True)
+    icon = models.CharField(null=True, max_length=255, blank=True)
+
     def __str__(self):
         return self.name
+
+
+class Package(models.Model):
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+    name = models.CharField(null=True, max_length=255, blank=True)
+    version = models.CharField(null=True, max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Module(models.Model):
+    package = models.ForeignKey('Package', on_delete=models.CASCADE, null=True)
+    name = models.CharField(null=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Script(models.Model):
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+    name = models.CharField(null=True, max_length=255, blank=True)
+    token = models.CharField(null=True, max_length=255, blank=True)
+    script_file = models.FileField(upload_to='api/uploads/')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        code = self.script_file.read()
+        tree = ast.parse(code)
+        for node in tree.body:
+            is_import = isinstance(node, ast.ImportFrom)
+            if not is_import:
+                continue
+            name = node.module
+            exists = Module.objects.filter(name=name).count() > 0
+            should_create = is_import and not exists
+            if should_create:
+                new_module = Module.objects.create(name=name)
+                new_module.save()
