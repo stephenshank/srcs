ae075df313c0953d7ce4096b430d5ce6e2dc6fef
stephenshank <sshank314@gmail.com>
2020-01-09 08:29:34 -0500

    Use card for spaced repetition in home component.

--- frontend/home.jsx
+++ frontend/home.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import SyntaxHighlighter from 'react-syntax-highlighter';
 import axios from "axios";
 
 function Home() {
@@ -12,36 +17,44 @@ function Home() {
   };
   useEffect(()=>fetchAnItem(), [0]);
   if(!item.id) return <div />;
-  return (<div>
-    <h3>{item.sheet.name}</h3>
-    <h4>{item.section.name}</h4>
-    <p>{item.name}:</p>
-    {!assessed ? <Button
-      variant="primary"
-      onClick={()=>setAssessed(true)}
-    >Assess</Button> : null }
-    {assessed ? (<>
-      <p>{item.shortcut}</p>
-      <Button
-        variant="primary"
-        onClick={() => {
-          setAssessed(false);
-          fetchAnItem();
-        }}
-      >
-          Continue
-      </Button>
-      <Button
-        variant="danger"
-        onClick={() => {
-          setAssessed(false);
-          fetchAnItem('remove', item.id);
-        }}
-      >
-          Remove and continue
-      </Button>
-    </>): null}
-  </div>);
+  return (<Container>
+    <Row>
+      <Col md={6}>
+        <Card>
+          <Card.Body>
+            <Card.Title>{item.sheet.name}</Card.Title>
+            <Card.Subtitle className="text-muted">{item.section.name}</Card.Subtitle>
+            <Card.Text>{item.name}:</Card.Text>
+            {!assessed ? <Button
+              variant="primary"
+              onClick={()=>setAssessed(true)}
+            >Assess</Button> : null }
+            {assessed ? (<>
+              <SyntaxHighlighter>{item.shortcut}</SyntaxHighlighter>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  setAssessed(false);
+                  fetchAnItem();
+                }}
+              >
+                  Continue
+              </Button>
+              <Button
+                variant="danger"
+                onClick={() => {
+                  setAssessed(false);
+                  fetchAnItem('remove', item.id);
+                }}
+              >
+                  Remove and continue
+              </Button>
+            </>): null}
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>);
 }
 
 export default Home;
